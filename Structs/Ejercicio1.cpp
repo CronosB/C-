ca4c147a55c268cf483e6c900bea110c256aec92
Iago Barrera <iago.barrera.programmer@gmail.com>
2017-09-20 13:19:00 +0200

    Ejercicio de agenda reformulado

--- Structs/Ejercicio1.cpp
+++ Structs/Ejercicio1.cpp
@@ -2,122 +2,373 @@
 #include <string.h>
 
 
+struct phonebook {
+
+	int numberEntries;
+
+	char name [15];
+
+	int number;
+
+};
+
+
+
+void menu ();
+
+bool checkOption ( char option );
+
+void texts ( char choiceText );
+
+void releaseBufferInput ();
+
+struct phonebook *dataEntry ();
+
+void searchNumber ( struct phonebook *userPhonebook );
+
+void searchName ( struct phonebook *userPhonebook );
+
+
+
 int main () {
 
-	const int PHONEBOOK_LENGTH = 2;
+	menu ();
 
-	const int NAME_LENGTH = 15;
+	return 0;
 
-	char option;
-
-
-	struct phonebook {
-
-		char name[15];
-
-		unsigned int numberTelephone;
-
-	};
+}
 
 
-	phonebook test_Phonebook[PHONEBOOK_LENGTH];
+//Esta función genera el menú y permite al usuario elegir la opción que corresponda.
+
+void menu () {
+
+	char userOption = 0;
+
+	bool checkUserOption = false;
+
+	phonebook *userPhonebook = 0;
+
+	bool checkUsed = false;
 
 
+	while ( userOption != 'q' ) {
 
-	printf ( "Bienvenido a la agenda, a continuación introducirás los datos correspondientes para operar. \n" );
+		texts ( 'm' );
 
+		userOption = getchar ();
 
-	for ( int i = 0; i < PHONEBOOK_LENGTH; i++ ) {
+		releaseBufferInput ();
 
-		printf ( "Introduce un nombre. \n" );
-
-
-		scanf ( "%15s", test_Phonebook[i].name );
-
-
-		printf ( "Introduce su número de teléfono. \n" );
+		checkUserOption = checkOption ( userOption );
 
 
-		scanf ( "%9d", &test_Phonebook[i].numberTelephone );
+	//Este bucle comprueba el valor introducido por el usuario mediante el valor ansi de cada caracter.
 
+		while ( checkUserOption ) {
+
+			texts ( 'e' );
+
+			userOption = getchar ();
+
+			releaseBufferInput ();
+
+			checkUserOption = checkOption ( userOption );
 
 		}
 
 
-	printf ( "Menú: Elige una de las siguientes opciones. \n a) Buscar por nombre. \n b) Buscar por número de teléfono. \n c) Salir. \n" );
+		switch ( userOption ) {
+
+			case 'a':
+
+				if ( checkUsed == false ) {
+
+					userPhonebook = dataEntry ();
+
+					checkUsed = true;
+
+					break;
+
+			}
+
+			else {
+
+				texts ( 'u' );
+
+				break;
+
+			}
 
 
-	scanf ( "%1s", &option );
+			case 'b':
 
+				if ( checkUsed ) {
 
-	switch ( option ) {
-
-		case 'a':
-
-		{
-
-			char nameIndex[NAME_LENGTH];
-
-
-			printf ( "Introduce un nombre: \n" );
-
-
-			scanf ( "%15s", nameIndex );
-
-
-			for ( int j = 0; j < PHONEBOOK_LENGTH; j++ ) {
-
-
-				if ( strcmp ( nameIndex, test_Phonebook[j].name ) == 0 ) {
-
-					printf ( "El número de teléfono de %s, es %d. \n", nameIndex, test_Phonebook[j].numberTelephone );
+					searchNumber ( userPhonebook );
 
 					break;
 
 				}
 
-			}
+				else {
 
-		break;
-
-		}
-
-		case 'b':
-
-		{
-
-		unsigned	int numberTelephoneIndex;
-
-
-			printf ( "Introduce un número de teléfono: \n" );
-
-
-			scanf ( "%9d", &numberTelephoneIndex );
-
-
-			for ( int k = 0; k < PHONEBOOK_LENGTH; k++ ) {
-
-
-				if ( numberTelephoneIndex == test_Phonebook[k].numberTelephone ) {
-
-					printf( "El nombre a quien pertenece el número %d, es %s. \n", numberTelephoneIndex, test_Phonebook[k].name );
+					texts ( 'z' );
 
 					break;
 
 				}
 
-			}
 
-		break;
+			case 'c':
+
+				if ( checkUsed ) {
+
+					searchName ( userPhonebook );
+
+					break;
+				}
+
+				else {
+
+					texts ( 'z' );
+
+					break;
+
+				}
+
+
+			case 'q':
+
+				texts ( 'g' );
+
+				if ( checkUsed ) {
+
+					delete [] userPhonebook;
+
+				}
+
+				break;
 
 		}
-
-		case 'c':
-
-			printf( "Gracias por utilizarme.\n" );
 
 	}
 
-	return 0;
+}
+
+//Comprueba que la variable pasada a la función sea: "a", "b", "c" o "q" con sus valores correspondientes en ANSI.
+
+bool checkOption ( char option ) {
+
+	bool check = false;
+
+	check = (
+
+			option != 97 &&
+			option != 98 &&
+			option != 99 &&
+			option != 113
+
+		);
+
+	return check;
+
+}
+
+
+/*Esta función imprime en pantalla el texto elegido:
+"m" para el menú,
+"e" para el mensaje de error al introducir mal un caracter,
+"g" para la despedida,
+"d" para el mensaje de cantidad de argumentos para la agenda,
+"l" para el mensaje de introducción de nombre,
+"n" para el mensaje de introducción de número,
+"u" para indicar que ya se introdujeron datos al struct phonebook y
+"z" para informar que no se han introducido datos.
+*/
+
+void texts ( char choiceText ) {
+
+
+	switch ( choiceText ) {
+
+		case 'm' :
+
+			printf ( "Elige una de las siguientes opciones: \n a) Agregar números. \n b) Busca por nombre. \n c) Busca por número. \n q) Salir. \n \n" );
+
+			break;
+
+
+		case 'e' :
+
+			printf ( "Has introducido una opción incorrecta, por favor vuelve a intentarlo. \n" );
+
+			break;
+
+
+		case 'g' :
+
+			printf ( "Gracias por utilizarme. \n" );
+
+			break;
+
+
+		case 'd' :
+
+			printf ( "Indica la cantidad de nombres y números que quieres añadir, por favor. \n" );
+
+			break;
+
+
+		case 'l' :
+
+			printf ( "Introduce un nombre. \n" );
+
+			break;
+
+
+		case 'n' :
+
+			printf ( "Introduce un número. \n" );
+
+			break;
+
+
+		case 'u' :
+
+			printf ( "Ya has introducido los datos. \n" );
+
+			break;
+
+
+		case 'z' :
+
+			printf ( "No has introducido ningún dato. \n" );
+
+			break;
+
+	}
+
+}
+
+
+//Esta función vacía el buffer de entrada de posibles caracteres que añada el usuario a mayores y del salto de linea.
+
+void releaseBufferInput () {
+
+	char checkGetChar = 0;
+
+	do
+
+		checkGetChar = getchar();
+
+	while ( checkGetChar != '\n' && checkGetChar != EOF );
+
+}
+
+
+//Esta función recoge la cantidad de datos que introducirá el usuario y los correspondientes nombres y números.
+
+struct phonebook *dataEntry () {
+
+	int numberEntries = 0;
+
+
+
+	texts ( 'd' );
+
+	scanf ( "%d", &numberEntries);
+
+	releaseBufferInput ();
+
+
+	phonebook *userPhonebook = 0;
+
+	userPhonebook = new phonebook [numberEntries];
+
+
+	userPhonebook[0].numberEntries = numberEntries;
+
+
+	for ( int i = 0; i < numberEntries; i++ ) {
+
+		texts ( 'l' );
+
+		scanf ( "%15s", userPhonebook[i].name );
+
+		releaseBufferInput ();
+
+
+		texts ( 'n' );
+
+		scanf ( "%9d", &userPhonebook[i].number );
+
+		releaseBufferInput ();
+
+	}
+
+	return userPhonebook;
+
+}
+
+
+
+//Esta función compara dentro del array del struct el campo del nombre con el nombre que introduce el usuario e imprime el número de la misma posición dentro de ese array.
+
+void searchNumber ( struct phonebook *userPhonebook ) {
+
+	int numberEntries = userPhonebook[0].numberEntries;
+
+	char name[15];
+
+
+	texts ( 'l' );
+
+
+	scanf ( "%15s", name );
+
+	releaseBufferInput ();
+
+
+	for ( int i = 0; i < numberEntries; i++ ) {
+
+	 if ( strcmp ( name, userPhonebook[i].name ) == 0 ) {
+
+		 printf ( "El número correspondiente a %s es el %d. \n", userPhonebook[i].name, userPhonebook[i].number );
+
+	 }
+
+	}
+
+}
+
+
+
+//Esta función compara dentro del array del struct el campo del número con el número que introduce el usuario e imprime el nombre de la misma posición dentro de ese array.
+
+void searchName ( struct phonebook *userPhonebook ) {
+
+	int numberEntries = userPhonebook[0].numberEntries;
+
+	int number;
+
+
+	texts ( 'n' );
+
+
+	scanf ( "%d", &number );
+
+	releaseBufferInput ();
+
+
+	for ( int i = 0; i < numberEntries; i++ ) {
+
+		if ( number == userPhonebook[i].number ) {
+
+			printf ( "El nombre correspondiente al número %d, es de %s. \n", number, userPhonebook[i].name );
+
+		}
+
+	}
 
 }

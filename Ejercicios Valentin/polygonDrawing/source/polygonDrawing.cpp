83c65f3d5fd8e2de9068d5f2214c5a6de9df1423
Iago <iago.barrera.programmer@gmail.com>
2017-10-13 20:22:13 +0200

    Rediseñada la función isNumber y checkData. Arreglado un error en printScreen que no dibujaba bien en pantalla la figura. Además he modificado el struct para añadir un campo llamado polygonType que recoje el tipo de polígono pasado como 2 argumento en el programa. Falta por rediseñar la función choicePolygon y revisar los errores que devuelve valgrind.

--- Ejercicios Valentin/polygonDrawing/source/polygonDrawing.cpp
+++ Ejercicios Valentin/polygonDrawing/source/polygonDrawing.cpp
@@ -12,6 +12,8 @@ struct polygon {
 
   int height;
 
+  const char *polygonType;
+
   bool **polygon;
 
 };
@@ -20,13 +22,13 @@ struct polygon {
 
 bool isNumber ( const char **userArguments );
 
-polygon *reserveMemory ( const char **userArguments );
+polygon *reserveMemory ( const char *typePolygon, const char *base, const char *height );
 
 void freeMemory ( polygon *userPolygon );
 
-bool checkData ( int argumentsLenght, const char **userArguments );
+bool checkData ( int argumentsLenght, const char *base, const char *height );
 
-void choicePolygon ( const char **userArguments );
+void choicePolygon ( polygon *userPolygon );
 
 polygon *square ( polygon *userPolygon );
 
@@ -37,14 +39,13 @@ void drawPolygon ( polygon *userPolygon );
 
 int main(int argc, char const *argv[]) {
 
-  if ( !checkData ( argc, argv ) ) {
+  if ( !checkData ( argc, argv[2], argv[3] ) ) {
 
     return 0;
 
   }
 
-  choicePolygon ( argv );
-
+  choicePolygon ( reserveMemory ( argv[1], argv[2], argv[3] ) );
 
 
   return 0;
@@ -53,11 +54,16 @@ int main(int argc, char const *argv[]) {
 
 
 
-bool isNumber ( const char **userArguments ){
+/*
+ * Comprueba que el argumento pasado a al función sea un número.
+ */
 
-  for ( unsigned int i = 0; i < strlen ( userArguments[2] ); i++) {
 
-    if ( !isdigit ( userArguments[2][i] ) ) {
+bool isNumber ( const char *word ){
+
+  for ( unsigned int i = 0; i < strlen ( word ); i++) {
+
+    if ( !isdigit ( word[i] ) ) {
 
       return false;
 
@@ -65,32 +71,28 @@ bool isNumber ( const char **userArguments ){
 
   }
 
-
-  for ( unsigned int i = 0; i < strlen ( userArguments[3] ); i++) {
-
-      if ( !isdigit ( userArguments[3][i] ) ) {
-
-        return false;
-
-      }
-
-    }
-
   return true;
 
 }
 
+/*
+ * Pide memoria dinámica al sistema para crear la estructura. Una vez hecho eso asigna los arrays
+ * correspondientes transformandolos en número, a las variables base y height. Después, con ellas, crear
+ * crea un array de dos dimensiones para crear la figura elegida como typePolygon.
+ */
 
 
-polygon *reserveMemory ( const char **userArguments ) {
+polygon *reserveMemory ( const char *polygonType, const char *base, const char *height ) {
 
   polygon *userPolygon = NULL;
-  
+
   userPolygon = new polygon;
 
-  userPolygon->base = atoi ( userArguments[2] );
+  userPolygon->polygonType = polygonType;
 
-  userPolygon->height = atoi ( userArguments[3] );
+  userPolygon->base = atoi ( base );
+
+  userPolygon->height = atoi ( height );
 
   userPolygon->polygon = new bool*[userPolygon->base];
 
@@ -108,6 +110,11 @@ polygon *reserveMemory ( const char **userArguments ) {
 
 
 
+/*
+ * Esta función libera la memoria dinámica utilizada.
+ */
+
+
 void freeMemory ( polygon *userPolygon ) {
 
   for ( int i = 0; i < userPolygon->base; i++ ) {
@@ -116,25 +123,31 @@ void freeMemory ( polygon *userPolygon ) {
 
   }
 
-  delete[] userPolygon->polygon;
-  
+  delete [] userPolygon->polygon;
+
   delete [] userPolygon;
 
 }
 
 
 
-bool checkData ( int argumentsLenght, const char **userArguments ) {
+/*
+ * Esta función comprueba que la cantidad de argumentos introducidos son correctos y que los que
+ * corresponden a la base y altura son números.
+ */
+
+
+bool checkData ( int argumentsLenght, const char *base, const char *height ) {
 
   if ( !( argumentsLenght == 4 ) ) {
 
-    cout << "No has introducido los argumentos correctos" << endl;
+    cout << "Has introducido más datos de los necesarios." << endl;
 
     return false;
 
   }
 
-  if ( !isNumber ( userArguments ) ) {
+  if ( !isNumber ( base ) || !isNumber ( height ) ) {
 
     cout << "No has introducido números para calcular la figura." << endl;
 
@@ -148,7 +161,13 @@ bool checkData ( int argumentsLenght, const char **userArguments ) {
 
 
 
-void choicePolygon ( const char **userArguments ) {
+/*
+ * Esta función realiza las operaciones con una función de polígono en concreto, según la opción elegida
+ * por el usuario, la cual almacena en typePolygon.
+ */
+
+
+void choicePolygon ( polygon *userPolygon ) {
 
   char userPolygonType = 's';
 
@@ -156,39 +175,60 @@ void choicePolygon ( const char **userArguments ) {
   switch ( userPolygonType ) {
 
 
-    case 's':
+    case square:
 
-      polygon *userPolygon = NULL;
+      if ( userPolygon->base != userPolygon->height ) {
 
-      userPolygon = reserveMemory ( userArguments );
+        cout << "Los dos lados deben ser iguales." << endl;
+
+        break;
+
+      }
 
       drawPolygon ( square ( userPolygon ) );
 
-      freeMemory ( userPolygon );
+      break;
 
-    break;
+
+    case rectangle:
+
+      if ( userPolygon->base == userPolygon->height ) {
+
+        cout << "Los datos son los de un cuadrado, no de un rectángulo." << endl;
+
+        break;
+
+      }
+
+      drawPolygon ( square ( userPolygon ) );
+
+      break;
+
+
+    case triangle:
+
+      break;
 
   }
-
 
 }
 
 
 
+/*
+ * Se adjudica a cada posición dentro del array un valor positivo o negativo para definir la figura
+ * de un cuadrado.
+ */
+
+
 polygon *square ( polygon *userPolygon ) {
-
-  if ( userPolygon->base != userPolygon->height ) {
-
-    cout << "Los dos lados deben ser iguales." << endl;
-
-  }
 
   for ( int i = 0; i <  userPolygon->base; i++ ) {
 
     for ( int j = 0; j < userPolygon->height; j++ ) {
 
 		userPolygon->polygon[i][j] = false;
-    
+
       if ( i == 0 || ( i + 1 ) == userPolygon->base ) {
 
         userPolygon->polygon[i][j] = true;
@@ -211,18 +251,34 @@ polygon *square ( polygon *userPolygon ) {
 
 
 
+/*
+ * Dibuja el carácter elegido según los valores positivos recogidos en polygon.
+ */
+
+
+
 void drawPolygon ( polygon *userPolygon ) {
 
   for ( int i = 0; i < userPolygon->base; i++ ) {
 
     for ( int j = 0; j < userPolygon->height; j++ ) {
 
-      if ( userPolygon->polygon[i][j] ) cout << "* ";
+      if ( userPolygon->polygon[i][j] ) {
+
+        cout << "* ";
+
+      } else {
+
+        cout << "  ";
+
+      }
 
     }
 
     cout << endl;
 
   }
+
+  freeMemory ( userPolygon );
 
 }
